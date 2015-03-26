fd648e05e967fd41166539abe72e372ca9908252
wangmin <17763768@qq.com>
2015-03-26 22:49:39 +0800

    sql

--- hualiang.sql
+++ hualiang.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : bendi
-Source Server Version : 50519
+Source Server         : yii2
+Source Server Version : 50528
 Source Host           : localhost:3306
 Source Database       : hualiang
 
 Target Server Type    : MYSQL
-Target Server Version : 50519
+Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2015-03-26 17:50:32
+Date: 2015-03-26 22:49:06
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -36,7 +36,7 @@ CREATE TABLE `article` (
   `created_at` int(11) DEFAULT NULL,
   `updated_at` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of article
@@ -49,6 +49,11 @@ INSERT INTO `article` VALUES ('20', '9', null, '1', '一九九二', null, null, 
 INSERT INTO `article` VALUES ('21', '9', null, '1', '二零一零', null, null, '0', '', '', '', '/uploads/files/Desert.jpg', null, null, null);
 INSERT INTO `article` VALUES ('23', '10', null, '1', '董事长 张原野', null, '<p>央视网消息（新闻联播）：中共中央政治局3月24日下午就深化司法体制改革、保证司法公正进行第二十一次集体学习。中共中央总书记习近平在主持学习时强调，深化司法体制改革，建设公正高效权威的社会主义司法制度，是推进国家治理体系和治理能力现代化的重要举措。公正司法事关人民切身利益，事关社会公平正义，事关全面推进依法治国。要坚持司法体制改革的正确政治方向，坚持以提高司法公信力为根本尺度，坚持符合国情和遵循司法规律相结合，坚持问题导向、勇于攻坚克难，坚定信心，凝聚共识，锐意进取，破解难题，坚定不移深化司法体制改革，不断促进社会公平正义。。</p>', '0', '', '', '', '/uploads/files/rw1.jpg', null, null, null);
 INSERT INTO `article` VALUES ('24', '10', null, '1', '总书记：习近平', null, '<p>969－1975年 陕西省<a target=\"_blank\" href=\"http://baike.baidu.com/view/975612.htm\" style=\"text-decoration: none; color: rgb(19, 110, 194);\">延川县</a>文安驿公社梁家河大队知青、党支部书记</p><p><br/><span class=\"description\" style=\"display: block; color: rgb(85, 85, 85); font-size: 12px; font-family: 宋体; word-wrap: break-word; word-break: break-all; line-height: 15px; padding: 8px 7px; min-height: 12px; border-top-width: 1px; border-top-style: solid; border-top-color: rgb(224, 224, 224);\">习近平</span></p><p>1975－1979年&nbsp;<a target=\"_blank\" href=\"http://baike.baidu.com/view/1563.htm\" style=\"text-decoration: none; color: rgb(19, 110, 194);\">清华大学</a>化工系基本有机合成专业学习</p><p>1979－1982年&nbsp;<a target=\"_blank\" href=\"http://baike.baidu.com/view/24640.htm\" style=\"text-decoration: none; color: rgb(19, 110, 194);\">国务院办公厅</a>、中央军委办公厅秘书（现役）</p><p>1982－1983年 河北省<a target=\"_blank\" href=\"http://baike.baidu.com/view/132969.htm\" style=\"text-decoration: none; color: rgb(19, 110, 194);\">正定县</a>委副书记</p><p>1983－1985年&nbsp;<a target=\"_blank\" href=\"http://baike.baidu.com/view/4112.htm\" style=\"text-decoration: none; color: rgb(19, 110, 194);\">河北省</a>正定县委书记，正定县武装部第一政委、党委第一书记</p><p><br/></p>', '0', '', '', '', '/uploads/files/rw1.jpg', null, null, null);
+INSERT INTO `article` VALUES ('25', '13', null, '1', '华粮定位', null, '<p>新华网仰光3月26日电 （记者张云飞）缅甸执政党报《联邦日报》26日报道说，经过一月有余的战事，缅甸军方已经控制整个果敢地区。\r\n\r\n报道说，缅甸军方已经在果敢地区发出通令，敦促地方武装放下武器，并承诺保证其安全。\r\n\r\n老街军管当局在电话中告诉新华社记者，截至24日晚，果敢地区所有主要战略高地都已被军队控制，老街局势相对稳定，一些商店已经开门营业。\r\n\r\n2月9日，缅甸政府军与地方武装在果敢地区爆发战事。持续至今的战事严重影响边境地区安全稳定。缅甸官方统计，交战双方有400余人伤亡</p>', '0', '', '', '', '', null, '1427375172', '1427375172');
+INSERT INTO `article` VALUES ('26', '13', null, '1', '华粮宗旨', null, '<p>22222222222222新华网仰光3月26日电 （记者张云飞）缅甸执政党报《联邦日报》26日报道说，经过一月有余的战事，缅甸军方已经控制整个果敢地区。 报道说，缅甸军方已经在果敢地区发出通令，敦促地方武装放下武器，并承诺保证其安全。 老街军管当局在电话中告诉新华社记者，截至24日晚，果敢地区所有主要战略高地都已被军队控制，老街局势相对稳定，一些商店已经开门营业。 2月9日，缅甸政府军与地方武装在果敢地区爆发战事。持续至今的战事严重影响边境地区安全稳定。缅甸官方统计，交战双方有400余人伤亡</p>', '0', '', '', '', '', null, '1427375211', '1427378376');
+INSERT INTO `article` VALUES ('27', '13', null, '1', '华粮使命', null, '<p>333333333333333新华网仰光3月26日电 （记者张云飞）缅甸执政党报《联邦日报》26日报道说，经过一月有余的战事，缅甸军方已经控制整个果敢地区。 报道说，缅甸军方已经在果敢地区发出通令，敦促地方武装放下武器，并承诺保证其安全。 老街军管当局在电话中告诉新华社记者，截至24日晚，果敢地区所有主要战略高地都已被军队控制，老街局势相对稳定，一些商店已经开门营业。 2月9日，缅甸政府军与地方武装在果敢地区爆发战事。持续至今的战事严重影响边境地区安全稳定。缅甸官方统计，交战双方有400余人伤亡</p>', '0', '', '', '', '', null, '1427375232', '1427378383');
+INSERT INTO `article` VALUES ('28', '15', null, '1', 'UC九游完成阿里手游业务整合 将发布新战略', null, null, '0', '', '', '', '', null, '1427380010', '1427380010');
+INSERT INTO `article` VALUES ('29', '15', null, '1', 'UC九游完成阿里手游业务整合222222222', null, '<p>22222222222222新浪科技讯 3月11日下午消息，新浪科技从接近九游的消息人士获悉，九游与阿里游戏的整合已经初步完成，即将下周一宣布新的移动游戏平台战略2014年初，阿里巴巴宣布推出手机游戏平台，除了利用手机淘宝、支付宝钱包等阿里旗下用户过亿的超级App入口开展游戏分发业务，同时独家代理运营《暖暖环游世界》、《愤怒的小鸟斯黛拉》等多款手游。</p><p>6月，阿里巴巴集团与UC优视联合宣布，UC优视全资融入阿里巴巴集团，并组建阿里移动事业群，新任事业群总裁俞永福表示，阿里移动事业群在成功整合阿里集团的浏览器和搜索业务之后，未来还将整合阿里集团其他相关业务和团队。</p><p>这些团队包括地图、移动游戏、移动阅读、应用分发等。一个月后，俞永福即出任高德移动互联网事业部总经理，正式接管互联网地图业务。在此之前，UC曾先后整合一搜团队推出神马搜索，整合淘宝浏览器推出UC浏览器PC版</p><p>2014年11月，阿里游戏官方宣布，将手游业务交由阿里移动事业群负责。据了解，九游已经登录手机淘宝和支付宝钱包，位置类似于京东和大众点评在微信的推广入口。</p><p>与360、91等渠道相比，九游以重度游戏用户集中、ARUP值高著称。据记者了解，2014年，九游为《天龙八部》、《全民奇迹》、《征途》等多款手游大作贡献的收入，在全渠道占比中都名列第一，《卧虎藏龙》首测当日，九游收入更是超全盘的60%。阿里集团在搜索、游戏这些垂直互联网业务上的路线逐渐清晰，就是向俞永福领衔的阿里巴巴移动事业群集中。(张楠)</p><p>与360、91等渠道相比，九游以重度游戏用户集中、ARUP值高著称。据记者了解，2014年，九游为《天龙八部》、《全民奇迹》、《征途》等多款手游大作贡献的收入，在全渠道占比中都名列第一，《卧虎藏龙》首测当日，九游收入更是超全盘的60%。阿里集团在搜索、游戏这些垂直互联网业务上的路线逐渐清晰，就是向俞永福领衔的阿里巴巴移动事业群集中。(张楠)</p>', '0', '', '', '', '', null, '1427380062', '1427380062');
 
 -- ----------------------------
 -- Table structure for `columns`
@@ -72,7 +77,7 @@ CREATE TABLE `columns` (
   PRIMARY KEY (`id`),
   KEY `model_f` (`model_id`),
   CONSTRAINT `model_f` FOREIGN KEY (`model_id`) REFERENCES `models` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of columns
@@ -83,8 +88,8 @@ INSERT INTO `columns` VALUES ('3', '0', '4', '2', '农博会', 'aa', '3', '1', '
 INSERT INTO `columns` VALUES ('8', '1', '1', '1', '华粮溯源', '123', '1', '2', '1', '<p>aaa</p>', '', '10', '1426898856', '1426898856');
 INSERT INTO `columns` VALUES ('9', '1', '9', '1', '企业荣誉', '123', '3', '2', '1', '<p>爱爱爱</p>', '', '10', '1426898887', '1426898887');
 INSERT INTO `columns` VALUES ('10', '1', '2', '1', '团队介绍', '12', '2', '1', '1', '<p>11</p>', '', '10', '1426898960', '1426898960');
-INSERT INTO `columns` VALUES ('13', '1', '1', '1', '品牌理念', 'q\'q\'q', '4', '1', '1', '<p>12</p>', '', '10', '1427082642', '1427082642');
-INSERT INTO `columns` VALUES ('14', '1', null, '1', '科研创新', null, '5', '1', '1', null, null, '10', '1427082745', '1427082745');
+INSERT INTO `columns` VALUES ('13', '1', '10', '1', '品牌理念', 'q\'q\'q', '4', '1', '1', '<p>12</p>', '', '10', '1427082642', '1427082642');
+INSERT INTO `columns` VALUES ('14', '1', '11', '1', '科研创新', '122', '5', '1', '1', '<p>123</p>', '', '10', '1427082745', '1427082745');
 INSERT INTO `columns` VALUES ('15', '1', '6', '1', '集团新闻', '12', '6', '2', '1', '<p>34</p>', '', '10', '1427082784', '1427082784');
 INSERT INTO `columns` VALUES ('16', '1', null, '1', '媒体报道', null, '7', '2', '1', null, null, '10', '1427082803', '1427082803');
 INSERT INTO `columns` VALUES ('17', '2', null, '1', '国家政策', null, '1', '2', '1', null, null, '10', '1427082914', '1427082914');
@@ -176,10 +181,10 @@ CREATE TABLE `photo` (
 INSERT INTO `photo` VALUES ('1', '啊飒飒的', '/uploads/files/Desert.jpg', '11', null, '1234', null, '1', '1427250372', '1427250372');
 INSERT INTO `photo` VALUES ('2', '皮草2', '/uploads/files/Desert.jpg', '11', null, '123', null, '345', '1427250601', '1427250601');
 INSERT INTO `photo` VALUES ('3', 'tp1', '/uploads/files/Desert.jpg', '11', null, '344', null, '5', '1427259724', '1427334682');
-INSERT INTO `photo` VALUES ('4', '奖牌一', '/uploads/files/ry1.jpg', '18', null, '123', null, '12', '1427356201', '1427361891');
+INSERT INTO `photo` VALUES ('4', '奖牌一', '/uploads/files/ry1.jpg', '18', null, '123', null, '12', '1427356201', '1427371805');
 INSERT INTO `photo` VALUES ('5', '奖牌2', '/uploads/files/ry1.jpg', '18', null, '2', null, '1', '1427356233', '1427362952');
-INSERT INTO `photo` VALUES ('6', '奖牌3', '/uploads/files/ry2.jpg', '20', null, '44', null, '1', '1427362882', '1427362970');
-INSERT INTO `photo` VALUES ('7', '奖牌4', '/uploads/files/ry1.jpg', '21', null, '1', null, '2', '1427362916', '1427362979');
+INSERT INTO `photo` VALUES ('6', '奖牌3', '/uploads/files/ry2.jpg', '20', null, '44', null, '1', '1427362882', '1427371828');
+INSERT INTO `photo` VALUES ('7', '奖牌4', '/uploads/files/ry1.jpg', '21', null, '1', null, '2', '1427362916', '1427371838');
 
 -- ----------------------------
 -- Table structure for `photo_column`
@@ -217,7 +222,7 @@ CREATE TABLE `template` (
   `name` varchar(100) NOT NULL,
   `ename` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of template
@@ -231,6 +236,8 @@ INSERT INTO `template` VALUES ('6', '集团新闻', 'jtxw');
 INSERT INTO `template` VALUES ('7', '合作客户', 'hzkh');
 INSERT INTO `template` VALUES ('8', '联系我们', 'lxwm');
 INSERT INTO `template` VALUES ('9', '企业荣誉', 'qyry');
+INSERT INTO `template` VALUES ('10', '品牌理念', 'ppln');
+INSERT INTO `template` VALUES ('11', '科研创新', 'kycx');
 
 -- ----------------------------
 -- Table structure for `user`
