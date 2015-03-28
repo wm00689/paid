95ba8c36c91df79eea9b4520b33d2985e331c97a
wangmin <17763768@qq.com>
2015-03-28 22:39:11 +0800

    121212121212121212121212

--- backend/models/templateSearch.php
+++ backend/models/templateSearch.php
@@ -5,7 +5,7 @@ namespace backend\models;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use backend\models\template;
+use backend\models\Template;
 
 /**
  * templateSearch represents the model behind the search form about `backend\models\template`.
