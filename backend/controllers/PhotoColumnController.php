64d70100d63804342ab55385003afd5a956e4ea2
wangmin <17763768@qq.com>
2015-03-29 12:34:46 +0800

    dxx

--- backend/controllers/PhotoColumnController.php
+++ backend/controllers/PhotoColumnController.php
@@ -88,7 +88,7 @@ class PhotoColumnController extends BaseController
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['index', 'id' => $model->id]);
+            return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('update', [
                 'model' => $model,
