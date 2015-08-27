7c6ab36f957fcb67d8fd1afbdf289d19e9de252a
wm00689 <17763768@qq.com>
2015-08-27 09:16:56 +0800

    Signed-off-by: wm00689 <17763768@qq.com>

--- frontend/views/index/index.php
+++ frontend/views/index/index.php
@@ -36,7 +36,6 @@ foreach($cache['menu_19_photos'] as $photo)
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
     <meta name="description" content="<?= $cache['set']['description']?>"  />
     <meta name="keywords" content="<?= $cache['set']['keywords']?>"  />
